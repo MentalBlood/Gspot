da529836f34bd0a31b9697eb44ddf10f2dcd4b7a
Pedro Gimeno <pgimeno@email.fake>
2015-12-04 02:09:15 +0100

    Add legalese to main.lua too

--- main.lua
+++ main.lua
@@ -1,3 +1,14 @@
+-- Original author: https://github.com/trubblegum
+-- This is a modified version of https://github.com/trubblegum/Gspot/blob/cf0a49d7d2073686d7ddb32a4fa04e90593d36c4/main.lua
+-- The original program did not include a copyright notice.
+-- Modifications © Copyright 2015 Pedro Gimeno Fortea.
+--
+-- This software is provided 'as-is', without any express or implied warranty. In no event will the authors be held liable for any damages arising from the use of this software.
+-- Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
+-- 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+-- 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+-- 3. This notice may not be removed or altered from any source distribution.
+
 gui = require('Gspot') -- import the library
 --mainmenu = gui() -- create a gui instance. don't have to do this, but you may want a gui for each gamestate so they can talk to each other, and you won't have to recontsruct the gui every time you enter a state
 

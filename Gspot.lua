MentalBlood/Gspot

commit 1ebb18cf6dfec2f1d8ee45f52c99f85e0a3a933c

element:update() can remove an element (notably the feedback element removes itself on update()). This causes spurious mouse in/out events because of looping in the wrong direction. Fix by looping backwards in the update loop, also separating the mouse in/out loop to ensure the removed elements aren't considered.
While on it, loop backwards on the mouse in/out loop too, for performance.

Gspot.lua
@@ -137,7 +137,8 @@ Gspot.update = function(this, dt)
 			if bucket ~= element and bucket:containspoint(mouse) then this.mouseover = bucket end
 		end
 	end
-	for i, element in ipairs(this.elements) do
+	for i = #this.elements, 1, -1 do
+		local element = this.elements[i]
 		if element.display then
 			if element.update then
 				if element.updateinterval then
@@ -148,10 +149,15 @@ Gspot.update = function(this, dt)
 					end
 				else element:update(dt) end
 			end
+		end
+	end
+	for i = #this.elements, 1, -1 do
+		local element = this.elements[i]
+		if element.display then
 			if element:containspoint(mouse) then
 				if element.parent and element.parent:type() == 'Gspot.element.scrollgroup' and element ~= element.parent.scrollv and element ~= element.parent.scrollh then
-					if element.parent:containspoint(mouse) then this.mousein = element end
-				else this.mousein = element end
+					if element.parent:containspoint(mouse) then this.mousein = element break end
+				else this.mousein = element break end
 			end
 		end
 	end

commit 95ef12c1e24dc9c8fc558d8febb2d58afbe906cf

Fix mousein in update events
Commit 1ebb18c broke the mousein demo (the one in the hidden element that you have to press F1 to view, and when you hover your mouse it shows an incrementing counter).

That code calls the element's update function if defined. The group element doesn't define one, but the demo defines its own, and that code is responsible for invoking the update event of the demo.

The demo expected mousein to be properly set at that point, and since commit 1ebb18c moved the handling to a later loop, the event missed the mousein setting.

Fixed by changing the order of the loops: first the mousein checking loop runs, then the update event loop. It's still brittle, though. It depends on the element order, and inverting the loop sense changes the reported element, which shouldn't happen. In fact, the loop ran backwards after 1ebb18c, but now it runs forwards because otherwise the label steals the mousein when hovered and stops the counter.

Gspot.lua
@@ -137,6 +137,16 @@ Gspot.update = function(this, dt)
 			if bucket ~= element and bucket:containspoint(mouse) then this.mouseover = bucket end
 		end
 	end
+	for i = 1, #this.elements do
+		local element = this.elements[i]
+		if element.display then
+			if element:containspoint(mouse) then
+				if element.parent and element.parent:type() == 'Gspot.element.scrollgroup' and element ~= element.parent.scrollv and element ~= element.parent.scrollh then
+					if element.parent:containspoint(mouse) then this.mousein = element break end
+				else this.mousein = element break end
+			end
+		end
+	end
 	for i = #this.elements, 1, -1 do
 		local element = this.elements[i]
 		if element.display then
@@ -148,16 +158,6 @@ Gspot.update = function(this, dt)
 						element:update(dt)
 					end
 				else element:update(dt) end
-			end
-		end
-	end
-	for i = #this.elements, 1, -1 do
-		local element = this.elements[i]
-		if element.display then
-			if element:containspoint(mouse) then
-				if element.parent and element.parent:type() == 'Gspot.element.scrollgroup' and element ~= element.parent.scrollv and element ~= element.parent.scrollh then
-					if element.parent:containspoint(mouse) then this.mousein = element break end
-				else this.mousein = element break end
 			end
 		end
 	end